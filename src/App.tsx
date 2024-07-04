043c5954275b8fe1d24dd6d000bc36665c28d380
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2024-07-04 23:03:17 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from '@/components/Header'
+import Hoc from './hooks/Hoc'
+
+function App() {
+
+  return (
+      <div>
+        <Header/>
+        <Hoc/>
+      </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  define: {
+    // This line is not necessary, Vite handles environment variables differently
+    // "process.env": process.env,
+  },
+});
